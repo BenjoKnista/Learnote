d188a380fcb1935d43f13781c1c5a7eade86aeb3
BenjoKnista <231873162+BenjoKnista@users.noreply.github.com>
2025-11-16 00:22:44 +0100

    V0.1

---
+++ main.py
@@ -0,0 +1,83 @@
+import json
+import os
+
+jsondatei = "learnote.json"
+
+zu_lernen = []
+
+def laden():
+    global zu_lernen
+    if os.path.exists(jsondatei):
+        datei = open(jsondatei, "r")
+        zu_lernen = json.load(datei)
+        datei.close()
+
+    else:
+        print("Das hat leider nicht geklappt. Eine leere Liste wird nun ausgegeben.")
+
+
+def speichern():
+    global zu_lernen
+    datei = open(jsondatei, "w")
+    json.dump(zu_lernen, datei)
+    datei.close()
+
+
+def hinzufuegen():
+    global zu_lernen
+    titel = input("Titel: ")
+    beschreibung = input("Beschreibung: ")
+
+    einzelne_themen = {
+        "titel": titel,
+        "beschreibung": beschreibung,
+    }
+
+    zu_lernen.append(einzelne_themen)
+    speichern()
+    print("Gespeichert!")
+
+
+def loeschen():
+    global zu_lernen
+
+    anzeigen()
+
+    nummer = int(input("Welcher Eintrag soll gelöscht werden: \n"))
+
+    zu_lernen.pop(nummer - 1)
+    speichern()
+    print("+" * 50)
+    print("\nErfolgreich gelöscht!\n")
+    print("+" * 50)
+
+def anzeigen():
+        print("+++GESPEICHERTE THEMEN")
+        nummerierung = 1
+        for thema in zu_lernen:
+            print(f"{nummerierung}. {thema['titel']} - {thema['beschreibung']}")
+            nummerierung += 1
+
+
+laden()
+
+while True:
+    print("1. Thema hinzufügen")
+    print("2. Themen anzeigen")
+    print("3. Thema löschen")
+    print("4. Beenden")
+
+    auswahl = input("Deine Wahl: ")
+
+    if auswahl == "1":
+        hinzufuegen()
+    elif auswahl == "2":
+        anzeigen()
+        break
+    elif auswahl == "3":
+        loeschen()
+    elif auswahl == "4":
+        print("Alles klar! Bis zum nächsten mal!")
+        break
+    else:
+        print("Ungültige Eingabe! Bitte 1 - 3 wählen.")
